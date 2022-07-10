84f40e19b48da425294953b756c73e9e2b79fc8e
nandita01 <nanditarathore65@gmail.com>
2022-07-10 16:12:17 +0530

    commit by nandita

--- Abc.java
+++ Abc.java
@@ -2,8 +2,9 @@ public class Abc
 {
 public static void main(String[] args)
 {
-System.out.println("Hello World");
+
 System.out.println("karan kande");
+System.out.println("Nandita Rathore");
 }
 
 }
